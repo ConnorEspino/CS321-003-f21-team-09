a6b2dd143fa033b6f1b2aabe1083b338cead7222
Ken Reinier <kenaireinier@u.boisestate.edu>
2021-11-12 13:35:32 -0700

    Implements #6

---
+++ src/main/java/cs321/create/DNA.java
@@ -0,0 +1,17 @@
+package cs321.create;
+
+/**
+ * A holding class for constants to represent the DNA bases: A, T, C and G.
+ * Note that values are assigned such that A and T are binary complements of each other 
+ * and so are C and G. This simplifies some of the processing of DNA sequences.
+ * 
+ * @author amit
+ *
+ */
+public class DNA
+{
+    public static final int A = 0b00; // 0  Adenine
+    public static final int T = 0b11; // 3  Thiamine
+    public static final int C = 0b01; // 1  Cytosine
+    public static final int G = 0b10; // 2  Guanine
+}
