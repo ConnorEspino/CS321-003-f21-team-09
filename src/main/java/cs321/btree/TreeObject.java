25dd3dc4c73bb23cb7c7d80ea1dd336728e2eeb2
Jacob Swierstra <JacobSwierstra@u.boisestate.edu>
2021-11-12 13:16:54 -0700

    push request #3

--- src/main/java/cs321/btree/TreeObject.java
+++ src/main/java/cs321/btree/TreeObject.java
@@ -1,23 +1,23 @@
 package cs321.btree;
 
 public class TreeObject<E> {
-//    int key;
-//    int pos;
-//    public TreeObject<E> left;
-//    public TreeObject<E> right;
-//    public TreeObject<E> parent;
-//
-//    public TreeObject(int key1, int pos1) {
-//        key = key1;
-//        pos = pos1;
-//        left = null;
-//        right = null;
-//        parent = null;
-//    }
-//
-//    public int getKey() {
-//        return key;
-//    }
+    int key;
+    int pos;
+    public TreeObject<E> left;
+    public TreeObject<E> right;
+    public TreeObject<E> parent;
+
+    public TreeObject(int key1, int pos1) {
+        key = key1;
+        pos = pos1;
+        left = null;
+        right = null;
+        parent = null;
+    }
+
+    public int getKey() {
+        return key;
+    }
 
 
 }
