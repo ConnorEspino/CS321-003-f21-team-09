4b29c5d5209b18b8cc2777ba69c690e2aad30d80
Ken Reinier <kenaireinier@u.boisestate.edu>
2021-11-13 12:19:33 -0700

    Implements #15

---
+++ src/main/java/cs321/create/DNASequence.java
@@ -0,0 +1,12 @@
+package cs321.create;
+
+/**
+ * A class used for holding the entirety of the DNA sequence that is given as 
+ * an input.
+ * 
+ * @author Ken R.
+ * 
+ */
+public class DNASequence {
+    
+}
