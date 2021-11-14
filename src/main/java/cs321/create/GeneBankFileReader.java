9ba135e9c49d6ef3ac967dadd29380a60a885702
Connor Espino <connorespino@u.boisestate.edu>
2021-11-13 23:56:37 -0700

    Implements #21

--- src/main/java/cs321/create/GeneBankFileReader.java
+++ src/main/java/cs321/create/GeneBankFileReader.java
@@ -1,23 +1,25 @@
 package cs321.create;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class GeneBankFileReader {
-    File file;
-    Scanner scan;
-    int seqLength;
+    private File file;
+    private Scanner scan;
+    private int seqLength;
 
     /**
      * Constructor for GeneBankFileReader class
      * 
-     * @param readFile File to read from
+     * @param readFile The name of the file to read from
      * @param length The length of each sequence to read from the file
      * @throws FileNotFoundException
      */
-    public GeneBankFileReader(File readFile, int length) throws FileNotFoundException{
-        file = readFile;
+    public GeneBankFileReader(String readFile, int length) throws FileNotFoundException{
+        file = new File(readFile);
         scan = new Scanner(file);
+        //scan.useDelimiter("");
         seqLength = length;
     }
 
@@ -27,7 +29,28 @@ public class GeneBankFileReader {
      * @return DNASequence The next sequence in the given file, null if no more sequences
      */
     public DNASequence getNextSequence(){
-        
+        StringBuilder build = new StringBuilder();
+        String next;
+
+        //Create a string of the DNA sequence of length seqLength
+        for(int i = 0; i < seqLength; i++){
+            next = scan.next();
+
+            //Check if the scanner has reached the end of the readable DNA sequence
+            if(next.equals("/")){
+                return null;
+            }
+
+            //If the next character is not a DNA base then decrement i and try adding the next character
+            if(!next.equals("a") && !next.equals("c") && !next.equals("t") && !next.equals("g")){
+                i--;
+                continue;
+            }
+
+            build.append(next);
+        }
+        //TODO Update once DNASequence Class is done
+        return(new DNASequence(build));
     }
 
     /**
@@ -35,7 +58,7 @@ public class GeneBankFileReader {
      * @return int The length of each sequence being read from the file
      */
     public int getSequenceLength(){
-
+        return -1;
     }
 
     /**
