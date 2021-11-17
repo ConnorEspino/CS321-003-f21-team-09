90013fcf15c927f4111f57e0ba06fbc4752e11be
Connor Espino <connorespino@u.boisestate.edu>
2021-11-16 17:04:38 -0700

    Implements #21

--- src/main/java/cs321/create/GeneBankFileReader.java
+++ src/main/java/cs321/create/GeneBankFileReader.java
@@ -66,7 +66,7 @@ public class GeneBankFileReader {
             build.append(next);
         }
         //TODO Update once DNASequence Class is done
-        return(new DNASequence(build));
+        return(new DNASequence(build.toString()));
     }
 
     /**
