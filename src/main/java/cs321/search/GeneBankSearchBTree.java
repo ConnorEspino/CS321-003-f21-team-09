fe7ff362c274a560dd503a416e2bb5a684e0aec6
Connor Espino <connorespino@u.boisestate.edu>
2021-11-25 03:06:40 -0700

    #47 In progress, need other classes implemented

--- src/main/java/cs321/search/GeneBankSearchBTree.java
+++ src/main/java/cs321/search/GeneBankSearchBTree.java
@@ -1,13 +1,60 @@
 package cs321.search;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import cs321.btree.BTree;
 import cs321.common.ParseArgumentException;
 import cs321.common.ParseArgumentUtils;
+import cs321.create.DNASequence;
 
 public class GeneBankSearchBTree {
+    private static int useCache;
+    private static int cacheSize;
+    private static int debugLevel;
+    private static LinkedListCache cache;
 
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello world from cs321.search.GeneBankSearchBTree.main");
+        if(args.length < 3){
+            //print usage message & exit
+        }
+        Scanner queryScan;
+        
+        File treeFile = new File(args[1]);
+        File queryFile = new File(args[2]);
+
+        try{
+            int useCache = Integer.parseInt(args[0]);
+            queryScan = new Scanner(queryFile);
+        }catch(Exception e){
+            //Print usage message & exit
+        }
+
+        //Set cache size
+        //TODO: Ask about implementing the cache properly
+        if(args.length > 3 && useCache == 1){
+            int cacheSize = Integer.parseInt(args[3]);
+            cache = new LinkedListCache(cacheSize);
+        }
+
+        //Set debug level
+        if(args.length > 4){
+            int debugLevel = Integer.parseInt(args[4]);
+        }else if(useCache == 0){
+            int debugLevel = Integer.parseInt(args[3]);
+        }
+        
+        //Open Btree file
+        //TODO: Implement correctly when BTree class is complete
+        BTree tree; //Equals the bTree file
+
+
+        //Search file for frequency of each query in the query file
+        while(queryScan.hasNextLine()){
+            String line = queryScan.nextLine();
+            System.out.println(line + ": " + frequencyOf(tree, new DNASequence(line).getInt()));
+        }
 
     }
 
@@ -24,7 +71,7 @@ public class GeneBankSearchBTree {
         //TODO correct when BTreeObject class is implemented
 
         // Iterate through each object stored in the current node
-        for (int i = 0; i < x.getNumNodes(); i++) {
+        for (int i = 0; i < x.getNumElements(); i++) {
 
             // If the value is less than the current element and the left child exists,
             // count frequency starting from the left child
