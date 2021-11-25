ee24f003b3168ce7b7c1028bbe9bf0ed79c91aab
Connor Espino <connorespino@u.boisestate.edu>
2021-11-25 13:06:17 -0700

    Implements #51

--- src/test/java/cs321/btree/TreeObjectTest.java
+++ src/test/java/cs321/btree/TreeObjectTest.java
@@ -1,4 +1,6 @@
 package cs321.btree;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import cs321.create.DNASequence;
@@ -14,5 +16,12 @@ public class TreeObjectTest {
         }
     }
 
+    @Test
+    public void TreeObject_getKey_binaryKey(){
+        TreeObject obj = new TreeObject(new DNASequence("AAAA"));
+        assertEquals((long) 00000000, obj.getKey());
+    }
+
+    
 
 }
