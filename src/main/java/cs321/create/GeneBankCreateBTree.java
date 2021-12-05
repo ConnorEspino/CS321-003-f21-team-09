4efbed462c5cab55c116b458b00320d9e05c7661
Jacob Swierstra <JacobSwierstra@u.boisestate.edu>
2021-12-05 14:35:49 -0700

    #8 continuing createBTree

--- src/main/java/cs321/create/GeneBankCreateBTree.java
+++ src/main/java/cs321/create/GeneBankCreateBTree.java
@@ -1,6 +1,7 @@
 package cs321.create;
 
 import cs321.btree.BTree;
+import cs321.btree.TreeObject;
 import cs321.common.ParseArgumentException;
 
 import java.io.*;
@@ -12,8 +13,13 @@ public class GeneBankCreateBTree {
         System.out.println("Hello world from cs321.create.GeneBankCreateBTree.main");
         GeneBankCreateBTreeArguments geneBankCreateBTreeArguments = parseArgumentsAndHandleExceptions(args);
         BTree createdBTree = new BTree(geneBankCreateBTreeArguments);
-
-        createdBTree.BTreeInsert(sequence);
+        String FileName = args[2];
+        GeneBankFileReader reader = new GeneBankFileReader(FileName, 0);
+        TreeObject sequence = null;
+        while(reader.hasNextSequence()) {
+            sequence = new TreeObject(reader.getNextSequence());
+            createdBTree.BTreeInsert(sequence);
+        }
     }
 
     private static GeneBankCreateBTreeArguments parseArgumentsAndHandleExceptions(String[] args) {
@@ -28,12 +34,18 @@ public class GeneBankCreateBTree {
     }
 
     private static void printUsageAndExit(String errorMessage) {
-        System.out.println("USAGE: <0/1(no/with Cache)> <degree> <gbk_file> <subsequence_length> [<cache_size>] [<debug_level>]");
+        System.out.println(errorMessage+ "USAGE: <0/1(no/with Cache)> <degree> <gbk_file> <subsequence_length> [<cache_size>] [<debug_level>]");
         System.exit(1);
     }
 
     public static GeneBankCreateBTreeArguments parseArguments(String[] args) throws ParseArgumentException {
-        boolean useCache = Boolean.parseBoolean(args[0]);
+        if(args[0].equals("1")){
+            boolean useCache = true;
+        } else if (args[0].equals("0")){
+            boolean useCache = false;
+        } else {
+            printUsageAndExit("Use 0 for no Cache, 1 for use Cache\n");
+        }
         int degree = Integer.parseInt(args[1]);
         String gbkFileName = args[2];
         int subsequenceLength = Integer.parseInt(args[3]);
